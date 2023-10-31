38abeeb7118dc8b55479a2705c658bc85e1802c3
amine0110 <mokhtari.amine1996@gmail.com>
2023-10-31 21:43:55 +0100

    adapted the call

---
+++ pycad/datasets/__init__.py
@@ -0,0 +1,4 @@
+from .png_to_txt_mc import PngToTxtConverterMC
+from .png_to_txt_ml import PngToTxtConverterML
+from .data_splitter import DataSplitter
+from .yolo_dataset_yaml import YOLODatasetYaml
---
+++ pycad/datasets/yolo_dataset_yaml.py
@@ -0,0 +1,42 @@
+
+
+
+class YOLODatasetYaml:
+    '''
+    This class creates the `dataset.yaml` file to train the YOLOv8 model.\n
+    `Params`:
+    - train_path: the path to training images and labels (you give just the directory that contains the two folders)
+    - valid_path: the path to valid images and labels (you give just the directory that contains the two folders)
+    - nc: the number of classes to train
+    - class_names: here you can enter as much as class names as you want to train, the number of class names should be equal to the number of classes given before\n
+
+    To create the dataset.yaml file, you need to call the method `create_yaml` and also give the path to the location to save the yaml file.\n
+    \n\n
+    #### Example usage:
+    from pycad.preprocess.utils import YOLODatasetYaml
+
+    config = YOLODatasetYaml("path/to/train", "path/to/valid", 3, "t1", "t2", "t3")\n
+    config.create_yaml("dataset.yaml")
+    '''
+    def __init__(self, train_path, valid_path, nc, *class_names):
+        self.train_path = train_path
+        self.valid_path = valid_path
+        self.nc = nc
+        self.names = []
+        self.set_class_names(*class_names)
+        
+    def set_class_names(self, *class_names):
+        if self.nc != len(class_names):
+            raise ValueError(f"The number of classes (nc={self.nc}) must be equal to the number of class names provided ({len(class_names)}).")
+        self.names = list(class_names)
+    
+    def save(self, save_path):
+        with open(save_path, 'w') as outfile:
+            outfile.write(f"train: {self.train_path}\n")
+            outfile.write(f"val: {self.valid_path}\n")
+            outfile.write("\n")  # Adding a blank line
+            outfile.write(f"nc: {self.nc}\n")
+            outfile.write("names: " + str(self.names).replace("'", '"') + "\n")
+            
+    def create_yaml(self, save_path):
+        self.save(save_path)
