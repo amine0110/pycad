9190ccf7b94724e1e59bb675d424e14d0691928a
Mokhtari Mohammed El Amine <mokhtari.amine1996@gmail.com>
2023-11-02 14:28:09 +0100

    added versions

--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@ setup(
     version='0.0.1',
     packages=find_packages(),
     install_requires=[
-        'numpy',
-        'tqdm',
+        'numpy==1.26.0',
+        'tqdm==4.66.1',
         'glob2',
-        'SimpleITK',
-        'pillow',
-        'pydicom',
-        'vtk',
-        'vedo',
-        'scikit-image',
-        'opencv-python',
+        'SimpleITK==2.3.0',
+        'pillow==10.0.1',
+        'pydicom==2.4.3',
+        'vtk==9.2.6',
+        'vedo==2023.4.6',
+        'scikit-image==0.22.0',
+        'opencv-python==4.8.1.78',
         'pytest-shutil',
-        'nibabel',
-        'scikit-learn'
-        # other libraries
+        'nibabel==3.2.2',
+        'scikit-learn==1.1.2'
+        # other libraries with their versions
     ],
 )
