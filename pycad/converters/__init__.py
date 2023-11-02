fdcb580360aea6ee6efefa2f15298a9fb955bd7f
Mokhtari Mohammed El Amine <mokhtari.amine1996@gmail.com>
2023-11-02 10:45:30 +0100

    Update __init__.py

--- pycad/converters/__init__.py
+++ pycad/converters/__init__.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2023 PYCAD
+# This file is part of the PYCAD library and is released under the MIT License:
+# https://github.com/amine0110/pycad/blob/main/LICENSE
+
+
 from .nifti_to_dicom import NiftiToDicomConverter
 from .nifti_to_png import NiftiToPngConverter
 from .nifti_to_stl import Nifti2StlConverter
