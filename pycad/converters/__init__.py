a2f47e62ca63a244ab79da94cef9b269e62f3278
amine0110 <mokhtari.amine1996@gmail.com>
2023-11-05 13:12:21 +0100

    features: nifti2nrrd

--- pycad/converters/__init__.py
+++ pycad/converters/__init__.py
@@ -7,3 +7,4 @@ from .nifti_to_dicom import NiftiToDicomConverter
 from .nifti_to_png import NiftiToPngConverter
 from .nifti_to_stl import Nifti2StlConverter
 from .dicom_to_nifti import DicomToNiftiConverter
+from .nifti_to_nrrd import NiftiToNrrdConverter
