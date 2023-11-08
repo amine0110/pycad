db19bf3f16a4db014ddfb967c32c09e22701395b
amine0110 <mokhtari.amine1996@gmail.com>
2023-11-08 16:51:47 +0100

    point to monai dataset json

--- pycad/datasets/__init__.py
+++ pycad/datasets/__init__.py
@@ -7,3 +7,4 @@ from .png_to_txt_mc import PngToTxtConverterMC
 from .png_to_txt_ml import PngToTxtConverterML
 from .data_splitter import DataSplitter
 from .yolo_dataset_yaml import YOLODatasetYaml
+from .monai_dataset_json import MONAIDatasetOrganizer
