f2be078839f9f4da608727e46768023242cdd724
amine0110 <mokhtari.amine1996@gmail.com>
2023-12-04 20:03:49 +0100

    simplify the import

---
+++ pycad/datasets/segmentation/TotalSegmentator/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) 2023 PYCAD
+# This file is part of the PYCAD library and is released under the MIT License:
+# https://github.com/amine0110/pycad/blob/main/LICENSE
+
+from .liver_datasets import LiverDataset
