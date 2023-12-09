a16223a7a82d4c9e38b817e65bdbc2b72f3e7c26
amine0110 <mokhtari.amine1996@gmail.com>
2023-12-09 16:03:36 +0100

    heart dataset added

--- pycad/datasets/segmentation/TotalSegmentator/__init__.py
+++ pycad/datasets/segmentation/TotalSegmentator/__init__.py
@@ -26,3 +26,4 @@ from .gluteus_medius_left_dataset import GluteusMediusLeftDataset
 from .gluteus_medius_right_dataset import GluteusMediusRightDataset
 from .gluteus_minimus_left_dataset import GluteusMinimusLeftDataset
 from .gluteus_minimus_right_dataset import GluteusMinimusRightDataset
+from .heart_dataset import HeartDataset
