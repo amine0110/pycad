4c8c7474548c2f65839ec573679f67c8c60f9635
amine0110 <mokhtari.amine1996@gmail.com>
2023-12-09 21:27:01 +0100

    lung lower lobe left dataset added

--- pycad/datasets/segmentation/TotalSegmentator/__init__.py
+++ pycad/datasets/segmentation/TotalSegmentator/__init__.py
@@ -43,3 +43,4 @@ from .kidney_cyst_left_dataset import KidneyCystLeftDataset
 from .kidney_cyst_right_dataset import KidneyCystRightDataset
 from .kidney_left_dataset import KidneyLeftDataset
 from .kidney_right_dataset import KidneyRightDataset
+from .lung_lower_lobe_left_dataset import LungLowerLobeLeftDataset
