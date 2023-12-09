ad87daa1bdcd8ab86873cacada540d8eeaf9669b
amine0110 <mokhtari.amine1996@gmail.com>
2023-12-09 16:25:45 +0100

    autochthon right dataset added

--- pycad/datasets/segmentation/TotalSegmentator/__init__.py
+++ pycad/datasets/segmentation/TotalSegmentator/__init__.py
@@ -8,6 +8,7 @@ from .adrenal_gland_left_dataset import AdrenalGlandLeftDataset
 from .aorta_dataset import AortaDataset
 from .atrial_appendage_left_dataset import AtrialAppendageLeftDataset
 from .autochthon_left_dataset import AutochthonLeftDataset
+from .autochthon_right_dataset import AutochthonRightDataset
 from .brachiocephalic_trunk_dataset import BrachiocephalicTrunkDataset
 from .brachiocephalic_vein_left_dataset import BrachiocephalicVeinLeftDataset
 from .brachiocephalic_vein_right_dataset import BrachiocephalicVeinRightDataset
