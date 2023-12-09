568c7d1b10cd815a994d6c32fe3f1fad9c88fea1
amine0110 <mokhtari.amine1996@gmail.com>
2023-12-09 15:14:21 +0100

    gluteus medius right dataset added

--- pycad/datasets/segmentation/TotalSegmentator/__init__.py
+++ pycad/datasets/segmentation/TotalSegmentator/__init__.py
@@ -23,3 +23,4 @@ from .gallbladder_dataset import GallbladderDataset
 from .gluteus_maximus_left_dataset import GluteusMaximusLeftDataset
 from .gluteus_maximus_right_dataset import GluteusMaximusRightDataset
 from .gluteus_medius_left_dataset import GluteusMediusLeftDataset
+from .gluteus_medius_right_dataset import GluteusMediusRightDataset
