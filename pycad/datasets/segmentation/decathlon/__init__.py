96740f321b43273ffa3352e2eb60adca231da159
amine0110 <mokhtari.amine1996@gmail.com>
2023-12-10 17:30:52 +0100

    lung tumor from decathlon

--- pycad/datasets/segmentation/decathlon/__init__.py
+++ pycad/datasets/segmentation/decathlon/__init__.py
@@ -9,3 +9,4 @@ from .heart_dataset import HeartDataset
 from .brain_tumor_dataset import BrainTumorDataset
 from .colon_dataset import ColonDataset
 from .spleen_dataset import SpleenDataset
+from .lung_tumor_dataset import LungTumorDataset
