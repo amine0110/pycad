8d072a9f9fac0edbeb430c370a168ea38786e327
amine0110 <mokhtari.amine1996@gmail.com>
2023-12-10 17:26:26 +0100

    spleen from decathlon added

--- pycad/datasets/segmentation/decathlon/__init__.py
+++ pycad/datasets/segmentation/decathlon/__init__.py
@@ -8,3 +8,4 @@ from .hippocampus_dataset import HippocampusDataset
 from .heart_dataset import HeartDataset
 from .brain_tumor_dataset import BrainTumorDataset
 from .colon_dataset import ColonDataset
+from .spleen_dataset import SpleenDataset
