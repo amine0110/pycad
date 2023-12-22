305426e2d4424cc72a2cdc134f3bdbfa8d19a829
amine0110 <mokhtari.amine1996@gmail.com>
2023-12-22 21:23:22 +0100

    vertabrae T4 dataset

--- pycad/datasets/segmentation/TotalSegmentator/__init__.py
+++ pycad/datasets/segmentation/TotalSegmentator/__init__.py
@@ -108,3 +108,4 @@ from .vertebrae_S1_dataset import VertebraeS1Dataset
 from .vertebrae_T1_dataset import VertebraeT1Dataset
 from .vertebrae_T2_dataset import VertebraeT2Dataset
 from .vertebrae_T3_dataset import VertebraeT3Dataset
+from .vertebrae_T4_dataset import VertebraeT4Dataset
