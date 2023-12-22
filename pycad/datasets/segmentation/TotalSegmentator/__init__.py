55aa5210e960fdb96cd04f9af84cbae8531fbc5c
amine0110 <mokhtari.amine1996@gmail.com>
2023-12-22 21:47:13 +0100

    vertebrae T11 dataset

--- pycad/datasets/segmentation/TotalSegmentator/__init__.py
+++ pycad/datasets/segmentation/TotalSegmentator/__init__.py
@@ -115,3 +115,4 @@ from .vertebrae_T7_dataset import VertebraeT7Dataset
 from .vertebrae_T8_dataset import VertebraeT8Dataset
 from .vertebrae_T9_dataset import VertebraeT9Dataset
 from .vertebrae_T10_dataset import VertebraeT10Dataset
+from .vertebrae_T11_dataset import VertebraeT11Dataset
