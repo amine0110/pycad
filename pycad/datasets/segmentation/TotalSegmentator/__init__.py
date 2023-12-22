9d9e9673e5d552a5cd428d55d32f60c85851ea52
amine0110 <mokhtari.amine1996@gmail.com>
2023-12-22 11:32:55 +0100

    rib left 8 dataset

--- pycad/datasets/segmentation/TotalSegmentator/__init__.py
+++ pycad/datasets/segmentation/TotalSegmentator/__init__.py
@@ -60,3 +60,4 @@ from .rib_left_4_dataset import RibLeft4Dataset
 from .rib_left_5_dataset import RibLeft5Dataset
 from .rib_left_6_dataset import RibLeft6Dataset
 from .rib_left_7_dataset import RibLeft7Dataset
+from .rib_left_8_dataset import RibLeft8Dataset
