b7681271463941f1029e54160f5e142d966cf6fc
amine0110 <mokhtari.amine1996@gmail.com>
2023-12-30 15:28:49 +0100

    kidney stones dataset added

---
+++ pycad/datasets/detection/diverse/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2023 PYCAD
+# This file is part of the PYCAD library and is released under the MIT License:
+# https://github.com/amine0110/pycad/blob/main/LICENSE
+
+
+from .kidney_stone_dataset import KidneyStoneDataset
